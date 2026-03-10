7478c2f142348d1b60c925ae0c6a02e97811d121
Arvis Zubkevičs <az2291@gnvsk.lv>
2026-03-11 00:00:45 +0200

    rechange

--- mcbot.js
+++ mcbot.js
@@ -1,30 +1,24 @@
-const http = require('http');
-const port = process.env.PORT || 8080;
-
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Bot is running\n');
-}).listen(port, () => {
-  console.log(`Web server listening on port ${port}`);
-});
-
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
   host: 'play.minesteal.xyz',
   username: 'MoneyTalks',
-  version: '1.21.1',
+  version: '1.20.1', // Most Minesteal-style servers prefer 1.20.1
   hideErrors: true,
   clientRoot: null, 
 })
 
+// 1. SILENCE PHYSICS IMMEDIATELY
 bot.on('inject_allowed', () => {
   bot.physics.enabled = false 
 })
 
+// 2. LOG ALL CHAT (To see if it says "Register" or "Banned")
 bot.on('messagestr', (message) => {
   console.log(`[CHAT] ${message}`)
 })
+
+// 3. HANDLE RESOURCE PACKS (Some proxies kick if you ignore these)
 bot.on('resource_pack', () => {
   bot.acceptResourcePack()
 })
@@ -33,8 +27,8 @@ bot.on('spawn', () => {
   setTimeout(() => {
     bot.chat('/register renars123 renars123')
     bot.chat('/login renars123')
-    setTimeout(() => { bot.physics.enabled = true }, 2000)
-  }, 3500)
+    setTimeout(() => { bot.physics.enabled = true }, 3000)
+  }, 5000)
 })
 
 bot.on('kicked', (reason) => {
